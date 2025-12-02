bbfe9b1ed2baefb18700793074c114b809b709fb
h34091sm <moodal.shamanth@gmail.com>
2025-12-02 15:40:00 +0000

    Sector volatility metrics have now been calculated

--- SQL_Queries/SECTOR_GROUPING.sql
+++ SQL_Queries/SECTOR_GROUPING.sql
@@ -1,7 +1,6 @@
 USE NIFTY_50;
 
-SELECT * FROM all_stocks_and_dates;
-SELECT * FROM stock_metadata;
-
-SELECT	asd.Stock_Date, asd.Symbol, sm.Industry, asd.Prev_Close, asd.Open_Price, asd.High, asd.Low, asd.Last_Price, asd.Close_Price, asd.VWAP, asd.Volume, asd.Turnover, asd.Trades, asd.Deliverable_Vol, asd.Deliverable_Percent, asd.RateOfReturn
-FROM 	all_stocks_and_dates asd INNER JOIN stock_metadata sm on asd.Symbol = sm.Symbol;
+SELECT		asd.Stock_Date, sm.Industry, avg(asd.RateOfReturn) as Avg_Daily_ROR
+FROM		all_stocks_and_dates asd INNER JOIN stock_metadata sm on asd.Symbol = sm.Symbol
+GROUP BY 	Industry, Stock_Date
+ORDER BY	Industry;
--- sector_stock_metrics.py
+++ sector_stock_metrics.py
@@ -1,4 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv("ALL_STOCKS_AND_DATES.csv")
-print(df)
+df = pd.read_csv("SECTOR_AVG_ROR.csv")
+
+# Monthly Volatility
+df["Stock_Date"] = pd.to_datetime(df["Stock_Date"])
+df["Month"] = df["Stock_Date"].dt.to_period("M")
+monthly_vol = df.groupby(["Industry", "Month"])["Avg_Daily_ROR"].std().reset_index(name="Monthly Volatility")
+
+# Yearly Volatility
+df["Year"] = df["Stock_Date"].dt.to_period("Y")
+yearly_vol = df.groupby(["Industry", "Year"])["Avg_Daily_ROR"].std().reset_index(name="Yearly Volatility")
+
+# Create new CSV files for Volatility
+monthly_vol.to_csv("sector_performance_tables/SECTOR_MONTHLY_VOLATILITY.csv", index=False)
+yearly_vol.to_csv("sector_performance_tables/SECTOR_YEARLY_VOLATILITY.csv", index=False)
