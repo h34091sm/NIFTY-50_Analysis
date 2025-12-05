7ad8db763fc12f98cdd71505e8959d5237c218c0
h34091sm <moodal.shamanth@gmail.com>
2025-12-05 12:04:51 +0000

    Created queries to measure growth in turnover and stock value

---
+++ SQL_Queries/SECTOR_GROWTH.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE VIEW Industry_Value_Turnover AS
+SELECT		sm.Industry, asd.Stock_Date, avg(asd.Close_Price) AS Avg_Close_Price, sum(asd.Turnover) as Total_Turnover
+FROM		all_stocks_and_dates_cleaned asd INNER JOIN stock_metadata sm on asd.Symbol = sm.Symbol
+GROUP BY 	Industry, Stock_Date
+ORDER BY	Industry;
+
+# View to get the data for the first reported and last reported date of each stock - closing price and turnover 
+CREATE OR REPLACE VIEW Industry_First_Last_Entries AS
+WITH FD AS  # FD short for First Date
+(
+	SELECT	Stock_Date, Industry, Avg_Close_Price, Total_Turnover, ROW_NUMBER() OVER(PARTITION BY Industry ORDER BY Stock_Date) AS RN
+	FROM	Industry_Value_Turnover
+), 
+LD AS	# LD short for Last Date
+(
+	SELECT	Stock_Date, Industry, Avg_Close_Price, Total_Turnover, ROW_NUMBER() OVER(PARTITION BY Industry ORDER BY Stock_Date DESC) AS RN
+	FROM	Industry_Value_Turnover
+)
+
+SELECT	FD.Industry, FD.Stock_Date AS First_Date, FD.Avg_Close_Price as First_Avg_Close, FD.Total_Turnover as First_Total_Turnover, 
+		LD.Stock_Date AS Last_Date, LD.Avg_Close_Price as Last_Avg_Close, LD.Total_Turnover as Last_Total_Turnover
+FROM 	FD INNER JOIN LD ON FD.Industry = LD.Industry
+WHERE	FD.RN=1 AND LD.RN=1;
+
+# Measure the change in stock value (close price) and turnover. Then measure the rate of change, because each stock has different total duration on NIFTY-50
+WITH Industry_Performance_Change AS
+(
+	SELECT	Industry, DATEDIFF(Last_Date, First_Date) as Total_Days, Last_Avg_Close - First_Avg_Close as Change_In_Avg_Price, Last_Total_Turnover - First_Total_Turnover as Change_In_Turnover
+	FROM	Industry_First_Last_Entries
+)
+SELECT	Industry, Total_Days, Change_In_Avg_Price / Total_Days AS Rate_Of_Price_Change, Change_In_Turnover / Total_Days AS Rate_Of_Turnover_Change
+FROM	Industry_Performance_Change; 
---
+++ SQL_Queries/STOCK_GROWTH.sql
@@ -0,0 +1,27 @@
+USE NIFTY_50;
+
+# View to get the data for the first reported and last reported date of each stock - closing price and turnover 
+CREATE OR REPLACE VIEW Stock_First_Last_Entries AS
+WITH FD AS  # FD short for First Date
+(
+	SELECT	Stock_Date, Symbol, Close_Price, Turnover, ROW_NUMBER() OVER(PARTITION BY Symbol ORDER BY Stock_Date) AS RN
+	FROM	all_stocks_and_dates_cleaned
+), 
+LD AS	# LD short for Last Date
+(
+	SELECT	Stock_Date, Symbol, Close_Price, Turnover, ROW_NUMBER() OVER(PARTITION BY Symbol ORDER BY Stock_Date DESC) AS RN
+	FROM	all_stocks_and_dates_cleaned
+)
+
+SELECT	FD.Symbol, FD.Stock_Date AS First_Date, FD.Close_Price as First_Close, FD.Turnover as First_Turnover, LD.Stock_Date AS Last_Date, LD.Close_Price as Last_Close, LD.Turnover as Last_Turnover
+FROM 	FD INNER JOIN LD ON FD.Symbol = LD.Symbol
+WHERE	FD.RN=1 AND LD.RN=1;
+
+# Measure the change in stock value (close price) and turnover. Then measure the rate of change, because each stock has different total duration on NIFTY-50
+WITH Stock_Performance_Change AS
+(
+	SELECT	Symbol, DATEDIFF(Last_Date, First_Date) as Total_Days, Last_Close - First_Close as Change_In_Stock_Price, Last_Turnover - First_Turnover as Change_In_Turnover
+	FROM	Stock_First_Last_Entries
+)
+SELECT	Symbol, Total_Days, Change_In_Stock_Price / Total_Days AS Rate_Of_Price_Change, Change_In_Turnover / Total_Days AS Rate_Of_Turnover_Change
+FROM	Stock_Performance_Change; 
